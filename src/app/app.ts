fcb102e3bc8bd34f2c90bc8803fc0f566bced4d9
taffikuddinkhan <taffikuddink@gmail.com>
2025-10-22 14:13:18 +0530

    made admin-dashboard

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard';
+import { StudentManagementComponent } from './components/employee-management/student-management';
+import { CourseManagementComponent } from './components/course-management/course-management';
+import { DepartmentManagementComponent } from './components/department-management/department-management';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,App,AdminDashboardComponent,StudentManagementComponent,CourseManagementComponent,DepartmentManagementComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
